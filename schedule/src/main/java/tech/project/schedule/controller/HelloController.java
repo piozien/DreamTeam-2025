1760f9e6736113c7b13b9fbc033d1df124a3c1e2
Piotr Zienowicz <115182798+piozien@users.noreply.github.com>
2025-03-10 12:50:40 +0100

    integration with FE

--- schedule/src/main/java/tech/project/schedule/controller/HelloController.java
+++ schedule/src/main/java/tech/project/schedule/controller/HelloController.java
@@ -3,6 +3,7 @@ package tech.project.schedule.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 @AllArgsConstructor
@@ -10,5 +11,10 @@ public class HelloController {
     @GetMapping("/hello")
     public String test() {
         return "Hello World!";
+    }
+
+    @GetMapping("/")
+    public RedirectView redirectToHello() {
+        return new RedirectView("/hello");
     }
 }
