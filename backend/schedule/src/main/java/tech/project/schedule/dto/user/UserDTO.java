da5f0f54c619ceb3dc46256984900bae8bf612a2
RymszewiczAnna <115213228+RymszewiczAnna@users.noreply.github.com>
2025-03-17 21:36:34 +0100

    [BE] DTO

--- backend/schedule/src/main/java/tech/project/schedule/dto/user/UserDTO.java
+++ backend/schedule/src/main/java/tech/project/schedule/dto/user/UserDTO.java
@@ -1,4 +1,12 @@
 package tech.project.schedule.dto.user;
 
-public record UserDTO() {
-}
+import tech.project.schedule.model.enums.GlobalRole;
+
+import java.util.UUID;
+
+public record UserDTO(
+        UUID id,
+        String name,
+        String email,
+        GlobalRole globalRole
+) {}
