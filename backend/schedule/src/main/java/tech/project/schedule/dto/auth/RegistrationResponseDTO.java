78cc9dfa9f7e3774558e3a769325c5f78d2907fe
RymszewiczAnna <115213228+RymszewiczAnna@users.noreply.github.com>
2025-04-23 11:56:23 +0200

    [BE] RegistrationResponseDTO comment

--- backend/schedule/src/main/java/tech/project/schedule/dto/auth/RegistrationResponseDTO.java
+++ backend/schedule/src/main/java/tech/project/schedule/dto/auth/RegistrationResponseDTO.java
@@ -1,5 +1,9 @@
 package tech.project.schedule.dto.auth;
 
+/**
+ * Data Transfer Object that contains the response message for user registration attempts.
+ * Returns a simple status message indicating registration success or failure.
+ */
 public record RegistrationResponseDTO(
         String message
 ) {
