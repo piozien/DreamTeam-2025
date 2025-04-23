80d0e0be7673e19716d2d78d4a6e6c50ed19ff3a
RymszewiczAnna <115213228+RymszewiczAnna@users.noreply.github.com>
2025-04-23 11:55:23 +0200

    [BE] RegistrationRequest comment

--- backend/schedule/src/main/java/tech/project/schedule/dto/auth/RegistrationRequest.java
+++ backend/schedule/src/main/java/tech/project/schedule/dto/auth/RegistrationRequest.java
@@ -3,6 +3,11 @@ package tech.project.schedule.dto.auth;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+
+/**
+ * Data Transfer Object that contains user information needed for registration.
+ * This class captures all required fields for creating a new user account in the system.
+ */
 @Getter
 @AllArgsConstructor
 public class RegistrationRequest {
