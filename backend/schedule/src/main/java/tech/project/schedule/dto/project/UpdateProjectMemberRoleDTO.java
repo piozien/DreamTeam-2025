c7242c87fc4a80a7756b8b556b212392a22f6ebd
RymszewiczAnna <115213228+RymszewiczAnna@users.noreply.github.com>
2025-04-27 20:47:45 +0200

    [BE] UpdateProjectMemberRoleDTO comment

--- backend/schedule/src/main/java/tech/project/schedule/dto/project/UpdateProjectMemberRoleDTO.java
+++ backend/schedule/src/main/java/tech/project/schedule/dto/project/UpdateProjectMemberRoleDTO.java
@@ -3,6 +3,10 @@ package tech.project.schedule.dto.project;
 import jakarta.validation.constraints.NotNull;
 import tech.project.schedule.model.enums.ProjectUserRole;
 
+/**
+ * Data Transfer Object used for updating a project member's role.
+ * Contains only the role field which is required for the update operation.
+ */
 public record UpdateProjectMemberRoleDTO(
         @NotNull(message = "Role is required")
         ProjectUserRole role
