3882d1c0f10445fda7c4126ce84354978bdda031
RymszewiczAnna <115213228+RymszewiczAnna@users.noreply.github.com>
2025-05-13 11:56:50 +0200

    [BE] UserDTO comment

--- backend/schedule/src/main/java/tech/project/schedule/dto/user/UserDTO.java
+++ backend/schedule/src/main/java/tech/project/schedule/dto/user/UserDTO.java
@@ -6,6 +6,12 @@ import java.util.UUID;
 
 import tech.project.schedule.model.enums.UserStatus;
 
+/**
+ * Data Transfer Object for user information.
+ * Used to transfer user data between the service layer and API controllers,
+ * particularly in API responses where sensitive information like passwords 
+ * should be excluded. Contains essential user attributes for display and identification.
+ */
 public record UserDTO(
         UUID id,
         String firstName,
