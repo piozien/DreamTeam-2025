0361578e446ad22940faefdb3c9c3442e0200769
RymszewiczAnna <115213228+RymszewiczAnna@users.noreply.github.com>
2025-05-13 12:09:04 +0200

    [BE] NotificationRepository comment

--- backend/schedule/src/main/java/tech/project/schedule/repositories/NotificationRepository.java
+++ backend/schedule/src/main/java/tech/project/schedule/repositories/NotificationRepository.java
@@ -9,6 +9,12 @@ import tech.project.schedule.model.user.User;
 import java.util.List;
 import java.util.UUID;
 
+/**
+ * Repository interface for accessing and managing Notification entities.
+ * Extends JpaRepository to leverage standard CRUD operations and pagination support
+ * for notifications in the database. Provides methods for retrieving notifications
+ * filtered by specific criteria.
+ */
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, UUID> {
     List<Notification> findByUser(User user);
